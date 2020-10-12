bcd17e0345f9b01b0816ba47c95ddc45d205c531
Christopher Shin <cjs97@duke.edu>
2020-10-11 22:17:18 -0700

    removed override from watormodel

--- src/Model/WaTorModel.java
+++ src/Model/WaTorModel.java
@@ -4,21 +4,7 @@ import java.util.List;
 
 public class WaTorModel extends Model{
 
-  private final Grid mainGrid;
-
   public WaTorModel(String fileName, String modelType, String fileOut) {
     super(fileName,modelType, fileOut);
-    mainGrid=this.getGridOfCells();
-  }
-
-  @Override
-  public void updateCells() {
-    for (int row = 0; row < mainGrid.getCellsPerColumn(); row++) {
-      for (int column = 0; column < mainGrid.getCellsPerRow(); column++) {
-        Cell currentCell = mainGrid.getCell(row,column);
-        List<Cell> cellNeighbors = mainGrid.getNeighbors(row, column, currentCell);
-        currentCell.updateState(cellNeighbors);
-      }
-    }
   }
 }
