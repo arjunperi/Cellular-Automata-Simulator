bf09b7d6002ec9a2dc8d81371c2cc7b08d14abb4
Arjun Peri <arjun.peri@duke.edu>
2020-10-22 21:59:07 -0400

    adding JavaDoc

--- src/Controller/ControllerException.java
+++ src/Controller/ControllerException.java
@@ -1,11 +1,25 @@
 package Controller;
 
+
+/**
+ * This class allows for custom exceptions to be throw in the Controller class.
+ *
+ * @author Arjun Peri
+ */
 public class ControllerException extends RuntimeException {
 
+  /**
+   * Allows throwing a ControllerException with custom message.
+   * @param message
+   */
   public ControllerException(String message) {
     super(message);
   }
 
+  /**
+   * Allows throwing a ControllerException with custom message and cause.
+   * @param message
+   */
   public ControllerException(String message, Throwable cause) {
     super(message, cause);
   }
