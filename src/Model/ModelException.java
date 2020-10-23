88e0c2109dc99091d093aff2a4221d3f9d337f37
Arjun Peri <arjun.peri@duke.edu>
2020-10-22 22:00:18 -0400

    Update ModelException.java

--- src/Model/ModelException.java
+++ src/Model/ModelException.java
@@ -8,10 +8,19 @@ package Model;
 
 public class ModelException extends RuntimeException {
 
+  /**
+   * Allows throwing a ModelException with custom message.
+   * @param message
+   */
   public ModelException(String message) {
     super(message);
   }
 
+
+  /**
+   * Allows throwing a ModelException with custom message and cause.
+   * @param message
+   */
   public ModelException(String message, Throwable cause) {
     super(message, cause);
   }
